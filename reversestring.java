13a62de635d3787d2e257ff7df038346a57b156a
Shambhuraje Sathe <shambhurajesathe98@gmail.com>
2026-07-10 14:30:02 +0530

    Add ReverseString class to reverse a string

---
+++ reversestring.java
@@ -0,0 +1,8 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String original = "Hello World"; 
+        String reversed = new StringBuilder(original).reverse().toString();        
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+}
