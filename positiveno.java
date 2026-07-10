fc3c025816d78b3e652af0e03e12e3d246c7c153
Shambhuraje Sathe <shambhurajesathe98@gmail.com>
2026-07-10 14:26:14 +0530

    Add NumberCheck class to check if a number is positive

---
+++ positiveno.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();        
+        if (number > 0.0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0.0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+                scanner.close();
+    }
+}
