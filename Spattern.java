b35c2e9deec499f1a14105b443829d8a4a7613de
Shambhuraje Sathe <shambhurajesathe98@gmail.com>
2026-07-16 09:48:56 -0700

    Create Spattern.java

---
+++ Spattern.java
@@ -0,0 +1,15 @@
+public class Spattern {
+    public static void main(String[] args) {
+        int size=5;
+        for(int i=0; i<size; i++){
+            for(int j=0; j<size; j++){
+                if(i==0 || j==0 && i<=2 || i==2 || j==4 && i>=2 || i==4 ) {
+                     System.out.print("*");
+                } else { 
+                    System.out.print(" ");
+               }
+            }
+            System.out.println( );
+        }
+    }
+}
